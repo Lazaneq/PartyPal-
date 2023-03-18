8f2d1778bd2c284d3f4b49e94ac0ce85cc318519
Lazaneq <lazanowski.artur@gmail.com>
2023-03-18 16:57:13 +0100

    Śmiga

--- app/src/main/java/com/party/partypal/SettingsActivity.java
+++ app/src/main/java/com/party/partypal/SettingsActivity.java
@@ -1,39 +1,31 @@
 package com.party.partypal;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.signin.internal.Storage;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.persistence.PersistenceStorageEngine;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -73,33 +65,20 @@ public class SettingsActivity extends AppCompatActivity {
 
         getUserInfo();
 
-        mProfileImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, 1);
+        mProfileImage.setOnClickListener(view -> {
+            Intent intent=new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, 1);
 
 
-            }
         });
 
 
-        mConfirm.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-                    public void onClick(View view) {
-                saveUserInformation();
-
-            }
-        });
-mBack.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
+        mConfirm.setOnClickListener(view -> saveUserInformation());
+    mBack.setOnClickListener(view -> {
         finish();
         return;
-    }
-});
+    });
 
     }
 
@@ -161,25 +140,16 @@ mBack.setOnClickListener(new View.OnClickListener() {
             bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
             byte[] data= baos.toByteArray();
             UploadTask uploadTask = filepath.putBytes(data);
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    finish();
-                }
-            });
+            uploadTask.addOnFailureListener(e -> finish());
 
-            filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                @Override
-                public void onSuccess(Uri uri) {
-                    String downloadUrl = uri.toString();
-                    Map userInfo = new HashMap();
-                    userInfo.put("profileImageUrl", downloadUrl);
-                    mCustomerDatabase.updateChildren(userInfo);
-                    finish();
-                    return;
+            filepath.getDownloadUrl().addOnSuccessListener(uri -> {
+                String downloadUrl = uri.toString();
+                Map userInfo1 = new HashMap();
+                userInfo1.put("profileImageUrl", downloadUrl.toString());
+                mCustomerDatabase.updateChildren(userInfo1);
+                finish();
+                return;
 
-
-               }
            });
 
 
