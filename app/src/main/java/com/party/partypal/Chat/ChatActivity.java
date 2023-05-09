836cd77cbe7af2a929a04928f17bf7c2af9412a5
Kuba <jakub.migdalek@op.pl>
2023-05-09 22:34:17 +0200

    13 - wyświetlanie dopasowań

--- app/src/main/java/com/party/partypal/Chat/ChatActivity.java
+++ app/src/main/java/com/party/partypal/Chat/ChatActivity.java
@@ -1,13 +1,25 @@
 package com.party.partypal.Chat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.party.partypal.R;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class ChatActivity extends AppCompatActivity {
@@ -15,7 +27,14 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView.Adapter mChatAdapter;
     private RecyclerView.LayoutManager mChatLayoutManager;
 
-    private String currentUserID;
+    private EditText mSendEditText;
+
+    private Button mSendButton;
+
+    private String currentUserID, matchId, chatId;
+
+    DatabaseReference mDatabaseUser, mDatabaseChat;
+
 
 
     @Override
@@ -23,7 +42,14 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        matchId = getIntent().getExtras().getString("matchId");
+
         currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID).child("connections").child("matches").child(matchId).child("chatId");
+        mDatabaseChat = FirebaseDatabase.getInstance().getReference().child("Chat");
+
+        getChatId();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
@@ -32,7 +58,50 @@ public class ChatActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mChatLayoutManager);
         mChatAdapter = new ChatAdapter(getDataSetChat(),ChatActivity.this);
         mRecyclerView.setAdapter(mChatAdapter);
+
+        mSendEditText = findViewById(R.id.message);
+        mSendButton = findViewById(R.id.send);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
     }
+
+    private void sendMessage() {
+        String sendMessageText = mSendEditText.getText().toString();
+
+        if(!sendMessageText.isEmpty()){
+            DatabaseReference newMassageDb = mDatabaseChat.push();
+
+            Map newMassage = new HashMap();
+            newMassage.put("createdByUser",currentUserID);
+            newMassage.put("text",sendMessageText);
+
+            newMassageDb.setValue(newMassage);
+        }
+        mSendEditText.setText(null);
+    }
+
+    private void getChatId(){
+        mDatabaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    chatId = dataSnapshot.getValue().toString();
+                    mDatabaseChat = mDatabaseChat.child(chatId);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+    }
+
     private ArrayList<ChatObject> resultsChat = new ArrayList<ChatObject>();
     private List<ChatObject> getDataSetChat() {
         return resultsChat;
